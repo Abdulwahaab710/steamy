e027de76fec383df4705fc4d79034500e856fc5e
Joseph Roque <contact@josephroque.ca>
2017-03-05 09:27:30 -0500

    Add action to get user last played

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,9 +10,9 @@ export default class Home extends Component {
         <div className='home-inner'>
           <Logo logoFill='#616283' className='home-logo' />
           <h1 className='home-title centered'>Stop grinding your gears and find gamers you mesh with</h1>
-          <form className='home-container'>
+          <form className='home-container' action="http://localhost:3000/userLastPlayed/">
             <p className='home-subtitle centered'>- Step 1 -</p>
-            <input className='home-input' type='text' placeholder='Enter your username' />
+            <input className='home-input' type='text' name='username' placeholder='Enter your username' />
             <p className='home-subtitle centered'>- Step 2 -</p>
             <button className='home-input' type='submit'>Hookup</button>
           </form>
